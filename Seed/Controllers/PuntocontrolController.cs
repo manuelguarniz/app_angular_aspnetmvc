4f91983179039bbd7e3ab15b4aad8b12bb047a09
Manuel Guarniz <cruzemg95@gmail.com>
2021-02-13 12:21:06 -0500

    nuevo contorlador

---
+++ Seed/Controllers/PuntocontrolController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http;
+
+namespace Seed.Controllers
+{
+    [RoutePrefix("api/puntocontrol")]
+    public class PuntocontrolController : ApiController
+    {
+        // GET: api/puntocontrol
+        [HttpGet]
+        [Route("listar")]
+        public IHttpActionResult Get()
+        {
+            try
+            {
+                List<string> listaVacia = new List<string>();
+                return Ok(listaVacia);
+            }
+            catch (Exception e)
+            {
+                return InternalServerError(e);
+            }
+        }
+    }
+}
